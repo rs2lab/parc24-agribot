f9c3012e2931c15af8827e03f80c640463724396
anaximeno <anaximenobrito@gmail.com>
2024-07-22 22:13:59 -0100

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'parc24_agribot'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,9 +13,9 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='anaximeno',
-    maintainer_email='anaximenobrito@gmail.com',
-    description='TODO: Package description',
+    maintainer='RS2LAB',
+    maintainer_email='rs2lab@unicv.cv',
+    description='Smart UniCV PARC 2024 Engineers League Agribot Agent',
     license='MIT',
     tests_require=['pytest'],
     entry_points={

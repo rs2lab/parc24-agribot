8fc16f827a18eab83d5881bc6fb3d6b99b37d569
ax2ucv <anaximeno.brito@student.unicv.edu.cv>
2024-07-28 01:50:44 -0100

    chore: Update image detection algorithms

--- parc24_agribot/image_capturer.py
+++ parc24_agribot/image_capturer.py
@@ -4,23 +4,25 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from .perceiver import SensorType
+from .constants import DEFAULT_QoS_PROFILE_VALUE
 
 
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
-        left_topic = os.getenv('LEFT_CAMERA_TOPIC', '/left_camera/image_raw')
-        right_topic = os.getenv('RIGHT_CAMERA_TOPIC', '/right_camera/image_raw')
-        #front_topic = os.getenv('FRONT_CAMERA_TOPIC', '/zed2_center_camera/image_raw')
+        left_topic = os.getenv('LEFT_CAMERA_TOPIC',SensorType.LEFT_CAM.value)
+        right_topic = os.getenv('RIGHT_CAMERA_TOPIC', SensorType.RIGHT_CAM.value)
+        front_topic = os.getenv('FRONT_CAMERA_TOPIC', SensorType.FRONT_CAM.value)
 
         self._cv_bridge = CvBridge()
         self._left_cam_capture_counter = 0
         self._right_cam_capture_counter = 0
         self._front_cam_capture_counter = 0
 
-        self.subscription_left = self.create_subscription(Image, left_topic, self.image_callback_left, 10)
-        self.subscription_right = self.create_subscription(Image, right_topic, self.image_callback_right, 10)
-        #self.subscription_front = self.create_subscription(Image, front_topic, self.image_callback_front, 10)
+        self.subscription_left = self.create_subscription(Image, left_topic, self.image_callback_left, DEFAULT_QoS_PROFILE_VALUE)
+        self.subscription_right = self.create_subscription(Image, right_topic, self.image_callback_right, DEFAULT_QoS_PROFILE_VALUE)
+        self.subscription_front = self.create_subscription(Image, front_topic, self.image_callback_front, DEFAULT_QoS_PROFILE_VALUE)
 
     def image_callback_left(self, msg):
         cv_image = self._cv_bridge.imgmsg_to_cv2(msg, desired_encoding ="bgr8")
